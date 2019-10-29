b186aef788071bb813c3bb9db37cfbfcc5996910
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-10-28 21:39:47 -0500

    Database setup

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE  DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers 
+(
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (id)
+)
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Crispy Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('BLT');
+INSERT INTO burgers (burger_name) VALUES ('Swiss and Ranch Burger');
