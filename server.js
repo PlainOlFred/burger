c1f317601746d9ce2be771a2cb9716ae82b557f3
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-11-02 17:44:53 -0500

    required  express, express-handlebars; set PORT to process.env.PORT: set up view engine to handlebars; start sever

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-const express = require('express');
+const 
+  express = require('express'),
+  exphbs = require('express-handlebars')
+  routes = require('./controllers/burgers_controller');
+
+let PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+//app settings
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+app.listen(PORT, function(){
+  console.log(`App listening on http://localhost:${PORT}`)
+});
 
