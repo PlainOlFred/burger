57cb71c7871e2e2e2707a1af8c4cbd112acc1c38
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-11-02 20:39:00 -0500

    created router with orm method for express express method

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,4 +4,38 @@ const
 
 const router = express.Router();
 
+router
+.route('/') 
+
+  .get( (req, res) => {
+    burger.selectAll( (data) => {
+      let burgers = {
+        burgers: data
+      };
+      
+      console.log(burgers);
+      res.render('index', burgers)
+    });
+  })
+
+  .post( (req, res) => {
+    burger.insertOne([
+      'burger_name', 'devoured'
+    ], [
+      req.body.burger_name, req.body.devoured
+
+    ], (result) => {
+      res.status(200)
+
+    })
+
+  })
+
+  .put( (req, res) => {
+    burger.updateOne()
+
+  })
+  
+  
+
 module.exports = router;
