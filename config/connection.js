558cc1d2396a84a21c11835dc9c2d19314df1eb3
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-12-03 22:00:18 -0600

    heroku connection; fixed if statement

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,8 @@ const mysql = require('mysql');
 
 
 
-if(process.env.JAWSBD_URL) {
+if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-
 } else {
   connection = mysql.createConnection({
   host: 'localhost',
